dd900e3fb1a0ea145ba5b335efb77c6ef76af3e7
mariuszRep <72456467+mariuszRep@users.noreply.github.com>
2023-05-18 14:15:38 +0100

    Add files via upload

---
+++ metadataEntities.sql
@@ -0,0 +1,34 @@
+CREATE TABLE [dbo].[metadataEntities](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[metadataControlId] [int] NULL,
+	[pipelineParameters] [nvarchar](max) NULL,
+	[enabled] [bit] NULL
+)
+
+
+INSERT INTO [dbo].[metadataEntities]
+VALUES
+    ( 
+		1, 
+		N'{
+			"inputs_fileSystem": "metadata",
+			"outputs_fileSystem": "metadata",
+			"inputs_folderPath": "source",
+			"outputs_folderPath": "sink",
+			"fileName": "dboEmployees"
+		}', 
+		1 
+	),
+	( 
+		1, 
+		N'{
+			"inputs_fileSystem": "metadata",
+			"outputs_fileSystem": "metadata",
+			"inputs_folderPath": "source",
+			"outputs_folderPath": "sink",
+			"fileName": "dboOrders"
+		}', 
+		1 
+	)
+
+SELECT * FROM [dbo].[metadataEntities] WHERE [metadataControlId] = 1 AND [enabled] = 1 
